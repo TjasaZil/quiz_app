b8553dc3846180a904c7863bbcb3c87e4b818fe5
TjasaZil <tjasa.zilavec@gmail.com>
2024-09-29 21:15:39 +0200

    connected state

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 66, 4, 95),
-                  Color.fromARGB(255, 85, 7, 121),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen())),
-  ));
+  runApp(const Quiz());
 }
